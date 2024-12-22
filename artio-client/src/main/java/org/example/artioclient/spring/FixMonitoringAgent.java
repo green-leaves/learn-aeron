5074a977b24de80654207d92554ee05650030dbe
duytiep <duytiep@gmail.com>
2024-12-22 09:06:51 +0800

    Polishing fix library polling

--- artio-client/src/main/java/org/example/artioclient/spring/FixMonitoringAgent.java
+++ artio-client/src/main/java/org/example/artioclient/spring/FixMonitoringAgent.java
@@ -3,7 +3,6 @@ package org.example.artioclient.spring;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.agrona.concurrent.Agent;
-import org.agrona.concurrent.SleepingIdleStrategy;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,18 +11,17 @@ import org.springframework.stereotype.Component;
 public class FixMonitoringAgent implements Agent {
 
     private final FixLibraryContext libraryContext;
-    private final SleepingIdleStrategy sleepingIdleStrategy = new SleepingIdleStrategy(100L);
     private final SessionContext artioSession;
 
     @Override
     public int doWork() {
-        sleepingIdleStrategy.idle(libraryContext.getFixLibrary().poll(1));
+        int polled = libraryContext.getFixLibrary().poll(1);
         if (!artioSession.getSession().isConnected()) {
             log.info("Artio session is closed");
             artioSession.reset();
-            return 1;
+            return polled + 1;
         }
-        return 0;
+        return polled;
     }
 
     @Override
